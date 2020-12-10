308d9f13ac9bad9cd9818db2dd9818dce01211ed
Sunita752 <74800878+Sunita752@users.noreply.github.com>
2020-12-10 16:28:24 -0600

    Add files via upload

---
+++ Loop/src/Forloop.java
@@ -0,0 +1,28 @@
+
+public class Forloop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int a =1; a<8; a++) {
+			System.out.println("I love my family.");
+		}
+		
+		 for (int i = 0; i <= 10; i = i + 2) {
+		      System.out.println(i);
+		 }
+	}
+}
+			
+			
+			
+			
+			
+			
+	
+		
+	
+
+
+	
+
+
